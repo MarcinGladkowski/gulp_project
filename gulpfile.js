6dfda7bb539150277bc4e1ee2ded47618db64cd1
Martin89PL <marcingladkowski@gmail.com>
2016-11-07 19:16:44 +0100

    add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('reload', function(){
   browserSync.reload();
@@ -20,6 +21,9 @@ gulp.task('sass', function(){
   return gulp.src('src/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
