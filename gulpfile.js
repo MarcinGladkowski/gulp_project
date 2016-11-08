5db938781d0c05818a83bce00058ccf651d5c2eb
Martin89PL <marcingladkowski@gmail.com>
2016-11-08 22:14:03 +0100

    add htmlReplace htmlMin del sequence

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,29 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
+var htmlReplace = require('gulp-html-replace');
+var htmlMin = require('gulp-htmlmin');
+var del = require('del');
+var sequence = require('run-sequence');
+
+var config = {
+  dist: 'dist/',
+  src: 'src/',
+  cssin: 'src/css/**/*.css',
+  jsin: 'src/js/**/*.js',
+  imgin: 'src/img/**/*.{jpg,jpeg,png,gif}',
+  htmlin: 'src/*.html',
+  scssin: 'src/scss/**/*.scss',
+  cssout: 'dist/css/',
+  jsout: 'dist/js/',
+  imgout: 'dist/img/',
+  htmlout: 'dist/',
+  scssout: 'src/css/',
+  cssoutname: 'style.css',
+  jsoutname: 'script.js',
+  cssreplaceout: 'css/style.css',
+  jsreplaceout: 'js/script.js'
+};
 
 gulp.task('reload', function(){
   browserSync.reload();
@@ -16,41 +39,59 @@ gulp.task('reload', function(){
 gulp.task('serve', ['sass'], function(){
 
   browserSync({
-    server: 'src'
+    server: config.src
   })
 
-  gulp.watch('src/*.html', ['reload']);
-  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch([config.htmlin, config.jsin], ['reload']);
+  gulp.watch(config.scssin, ['sass']);
 });
 gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(config.scssin)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 3 versions']
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest(config.scssout))
     .pipe(browserSync.stream());
 });
 gulp.task('css', function(){
-  return gulp.src('src/css/**/*.css')
-    .pipe(concat('style.css'))
+  return gulp.src(config.cssin)
+    .pipe(concat(config.cssoutname))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest(config.cssout));
 });
 gulp.task('js', function(){
-  return gulp.src('src/js/**/*.js')
-    .pipe(concat('script.js'))
+  return gulp.src(config.jsin)
+    .pipe(concat(config.jsoutname))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest(config.jsout));
 });
 gulp.task('img', function(){
-  return gulp.src('src/img/**/*.{jpg,jpeg,png,gif}')
-    .pipe(changed('dist/img'))
+  return gulp.src(config.imgin)
+    .pipe(changed(config.imgout))
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest(config.imgout));
 });
-
+gulp.task('html', function(){
+  return gulp.src(config.htmlin)
+    .pipe(htmlReplace({
+      'css': config.cssreplaceout,
+      'js': config.jsreplaceout
+    }))
+    .pipe(htmlMin({
+      sortAttributes: true,
+      sortClassName: true,
+      collapseWhitespace: true,
+    }))
+    .pipe(gulp.dest(config.dist))
+});
+gulp.task('clean', function(){
+  return del([config.dist]);
+});
+gulp.task('build', function(){
+  sequence('clean', ['html', 'js', 'css', 'img']);
+});
 
 gulp.task('default',['serve']);
